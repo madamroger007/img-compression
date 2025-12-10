81263be322db15e81b9d8e7a3814433c3592e950
adam <104338817+madamroger007@users.noreply.github.com>
2025-12-11 05:50:51 +0700

    fix: change icon and label in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,12 @@
 import { Layers } from 'lucide-react';
+import Image from "next/image";
 
 export function Footer() {
   return (
     <footer className="py-6 px-8">
-      <div className="max-w-7xl mx-auto flex items-center justify-center gap-3">
-        <div className="p-2 rounded-lg bg-gradient-to-br from-blue-500 to-purple-600 shadow-md">
-          <Layers className="w-5 h-5 text-white" />
-        </div>
-        <span className="text-slate-600">ImagePro</span>
+      <div className="max-w-7xl mx-auto flex items-center justify-center gap-1">
+        <p className='text-slate-600 font-semibold'>Created by</p>
+        <Image alt='' src={"/icon.png"} width={40} height={40} />
       </div>
     </footer>
   );
